69e9f2a8fb76c79355406ec66ba7b9312c94254a
SteffanvanderWerf02 <steffan.van.der.werf@student.nhlstenden.com>
2022-10-06 16:38:47 +0200

    homescreen

---
+++ js/titlescreen.js
@@ -0,0 +1,22 @@
+new Typed('.typedJS',
+    {
+        strings: ["NHL Stenden Emmen", "Fake Mario", "Phaiser 3", "3EC's please "],
+        typeSpeed: 60,
+        shuffle: true,
+        backSpeed: 40,
+        backDelay: 2000,
+        startDelay: 0,
+        loop: true,
+        smartBackspace: true
+    }
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+    let begin = document.getElementById("StartGame");
+    let highscores = document.getElementById("Highscores");
+    let question = document.getElementById("newQuestions");
+
+    question.addEventListener("click", {
+
+    })
+})
