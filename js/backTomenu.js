fac0c13341726865e12f162d0b1b86276d33fbb2
SteffanvanderWerf02 <steffan.van.der.werf@student.nhlstenden.com>
2022-10-09 17:24:21 +0200

    create start game with name

--- js/backTomenu.js
+++ js/backTomenu.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let highscoreToMenu = document.getElementById("highscoreToMenu");
     let questionsToMenu = document.getElementById("questionsToMenu");
     let QuestionEditToQuestions = document.getElementById("QuestionEditToQuestions");
+    let gameNameToMenu = document.getElementById("gameNameToMenu");
 
     highscoreToMenu.addEventListener("click", () => {
         highScoresList.classList.add("d-none");
@@ -16,6 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     QuestionEditToQuestions.addEventListener("click", () => {
         document.getElementById("questions").classList.remove("d-none");
         document.getElementById("editQuestion").classList.add("d-none");
+    });
+
+    gameNameToMenu.addEventListener("click", () => {
+        document.getElementById("gameName").classList.add("d-none");
+        menu.classList.remove("d-none");
     });
 
 });
--- js/titlescreen.js
+++ js/titlescreen.js
@@ -12,10 +12,40 @@ new Typed('.typedJS',
 );
 
 document.addEventListener("DOMContentLoaded", () => {
-    let begin = document.getElementById("StartGame");
+    let begin = document.getElementById("StartGameSettings");
+    let StartGame = document.getElementById("StartGame");
 
 
-    begin.addEventListener("click", {
-
+    begin.addEventListener("click", () => {
+        document.getElementById("gameName").classList.remove("d-none");
+        menu.classList.add("d-none");
     })
+
+    StartGame.addEventListener("click", () => {
+        document.getElementById("gameName").classList.add("d-none");
+        document.getElementById("game").classList.remove("d-none");
+
+        setCookie("name", document.getElementById("gameNameInput").value, 0.5);
+    })
+
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    }
+
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
 })
